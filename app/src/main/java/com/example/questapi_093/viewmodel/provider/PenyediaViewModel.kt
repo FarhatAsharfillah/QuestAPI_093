c54bdb4c78105fa92b4d68d3c75a458fa5c23c12
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-12-18 19:21:15 +0700

    menambahkan viewmodel provider

--- app/src/main/java/com/example/questapi_093/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questapi_093/viewmodel/provider/PenyediaViewModel.kt
@@ -1,4 +1,23 @@
 package com.example.questapi_093.viewmodel.provider
 
-class PenyediaViewModel {
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.questapi_093.repositori.AplikasiDataSiswa
+import com.example.questapi_093.viewmodel.EntryViewModel
+import com.example.questapi_093.viewmodel.HomeViewModel
+
+fun CreationExtras.aplikasiDataSiswa():AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+                AplikasiDataSiswa
+        )
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiDataSiswa().container
+            .repositoryDataSiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().container
+            .repositoryDataSiswa) }
+    }
 }
