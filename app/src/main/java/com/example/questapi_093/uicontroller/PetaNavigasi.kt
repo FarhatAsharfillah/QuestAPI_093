27ccf3186e2f39f5222830ab7eda6eb98f220ec8
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-12-18 19:22:10 +0700

    menambahkan viewmodel provider

--- app/src/main/java/com/example/questapi_093/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi_093/uicontroller/PetaNavigasi.kt
@@ -1,4 +1,42 @@
 package com.example.questapi_093.uicontroller
 
-class PetaNavigasi {
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.questapi_093.uicontroller.route.DestinasiEntry
+import com.example.questapi_093.uicontroller.route.DestinasiHome
+import com.example.questapi_093.view.EntrySiswaScreen
+import com.example.questapi_093.view.HomeScreen
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(),
+                 modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route,
+        modifier = Modifier ){
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate
+                (DestinasiEntry.route) },
+                navigateToItemUpdate = {
+                    //    navController.navigate("${DestinasiDetail.route}/${it}")
+                })
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome
+                .route) })
+        }
+
+    }
 }
