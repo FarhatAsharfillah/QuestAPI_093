2564a559df397b5322b9014fec98e9aa7f6dab25
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-12-18 18:54:33 +0700

    membuat kode DestinasiHome

--- app/src/main/java/com/example/questapi_093/route/DestinasiHome.kt
+++ app/src/main/java/com/example/questapi_093/route/DestinasiHome.kt
@@ -1,4 +1,8 @@
 package com.example.questapi_093.route
 
-class DestinasiHome {
+import com.example.questapi_093.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
 }
