76d532578b735e013cf0d0f446f82bd464c862a0
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-12-25 21:30:42 +0700

    Menambahkan fungsi CRUD pada RepositoryDataSiswa

--- app/src/main/java/com/example/questapi_093/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_093/repositori/RepositoryDataSiswa.kt
@@ -3,10 +3,12 @@ package com.example.questapi_093.repositori
 import com.example.questapi_093.apiservice.ServiceApiSiswa
 import com.example.questapi_093.modeldata.DataSiswa
 
-interface RepositoryDataSiswa {
-    suspend fun getDataSiswa() : List<DataSiswa>
-
+interface RepositoryDataSiswa{
+    suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id:Int) : DataSiswa
+    suspend fun editSatuSiswa(id:Int,dataSiswa: DataSiswa) :retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
