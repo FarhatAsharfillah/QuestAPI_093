c3c6fc4edfa61fb5b3b35555fc7c18fbfd08aa49
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-12-25 21:14:36 +0700

    Menambahkan EditViewModel

---
+++ app/src/main/java/com/example/questapi_093/viewmodel/EditViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.questapi_093.viewmodel
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa = UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
