df5c9cd8ac7a1591623acd9b13441c42833f448b
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-12-25 21:22:19 +0700

    Penambahan import pada DetailViewModel

--- app/src/main/java/com/example/questapi_093/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_093/viewmodel/DetailViewModel.kt
@@ -1,8 +1,21 @@
 @file:OptIn(InternalSerializationApi::class)
 package com.example.questapi_093.viewmodel
 
-
-
+import android.annotation.SuppressLint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_093.modeldata.DataSiswa
+import com.example.questapi_093.repositori.RepositoryDataSiswa
+import com.example.questapi_093.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import kotlinx.serialization.InternalSerializationApi
+import java.io.IOException
+import retrofit2.HttpException
+import retrofit2.Response
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
